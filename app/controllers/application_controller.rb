cd9669883cbcec7c7099ac897261957a33f10cf3
hirokuni-k <shisenramen@yahoo.co.jp>
2021-02-13 14:52:53 +0900

    新規登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
+    #added_attrs = [:family_name, :first_name, :family_name_kana, :first_name_kana, :year, :month, :day]
+    #devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    #devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+  
+
+
+  #private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
