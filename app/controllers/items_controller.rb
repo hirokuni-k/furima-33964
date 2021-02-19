d9f5bcf4f0bbe43f1164c5e48bd25cb345c0014f
hirokuni-k <shisenramen@yahoo.co.jp>
2021-02-19 18:20:11 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,10 @@ class ItemsController < ApplicationController
     @items = Item.includes(:user).order("created_at DESC")
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
 
  def new
    @item = Item.new
