91a01f608fcd20c8f60a69c8e94e14f5fcfbfdf2
hirokuni-k <shisenramen@yahoo.co.jp>
2021-02-20 17:35:00 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
 
@@ -14,7 +15,7 @@ class ItemsController < ApplicationController
  end
 
  def create
-  @item = Item.new(item_params)
+  @item = Item.find(item_params)
   if @item.valid?
     @item.save
     redirect_to root_path
@@ -23,10 +24,37 @@ class ItemsController < ApplicationController
   end
  end
 
+ def edit
+ end
+
+def update
+  if @item.update(item_params)
+    redirect_to item_path 
+  else
+    render :edit
+  end
+end
+
 
 private
 
  def item_params
   params.require(:item).permit(:name, :image, :price, :description, :condition_id, :shipping_charge_id, :shipping_date_id, :prefecture_id, :category_id).merge(user_id: current_user.id)
  end
+
+
+ def set_item
+   @item = Item.find(params[:id])
+ end
+
+  def move_to_index
+   @item = Item.find(params[:id])
+   if current_user == @item.user
+     redirect_to action: :index
+   unless user_signed_in? && current_user.id == @item.user_id
+     redirect_to action: :index
+   end
+  end
+ end
 end
+ 
