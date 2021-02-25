adbe3c96325decf255f23227d2dffbc4a33cb3fd
hirokuni-k <shisenramen@yahoo.co.jp>
2021-02-25 16:45:41 +0900

    商品購入機能いろいろ

---
+++ app/models/purchase_shipping.rb
@@ -0,0 +1,20 @@
+class PurchaseShipping
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchase_id, :token, :price
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code,format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :prefecture_id,numericality: { other_than: 0, message: "Select" }
+    validates :city
+    validates :address
+    validates :phone_number,format: {with: /\A[0-9]+\z/},length: { maximum: 11 }
+    validates :token
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Shipping.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
