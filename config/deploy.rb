213a8ec5e29f07a1154033504cd4c126926efa09
hirokuni-k <shisenramen@yahoo.co.jp>
2021-03-02 14:18:12 +0900

    修正furima

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.16.0'
 set :application, 'furima-33964'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:Githubのユーザー名/レポジトリ名.git'
+set :repo_url,  'git@github.com:hirokuni_k/furima-33964.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
